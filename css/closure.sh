7a3c83cd8412f322c16fcdc527e0bc11f6dc9429
ldh35 <ldh35@cam.ac.uk>
2023-01-08 02:09:59 +0000

    Moved to minimised CSS and JS files

---
+++ css/closure.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cat card.css popup.css scrollbar.css > ka.css
---
+++ javascript/closure.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+uglifyjs -c -m -o ka.min.js --warn --keep-fnames --keep-fargs -b ascii_only=true -b beautify=false -- \
+  fittext.js \
+  Vec.js \
+  OverscrollDetector.js \
+  Animations.js \
+  Dealer.js \
+  CardFaceManager.js \
+  GridManager.js \
+  PopupManager.js \
+1> /dev/null
